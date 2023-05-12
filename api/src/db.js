d1d95127ebd2121296a244ce34c2e9adb780541a
Juan Jose Fiorovic <117408593+jjpicante@users.noreply.github.com>
2023-05-12 11:01:00 -0300

    Cambio sequelize para ejecutar en localhost

--- api/src/db.js
+++ api/src/db.js
@@ -11,19 +11,10 @@ const {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 }); */
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false,
-  native: false,
-  dialectOptions: {
-    ssl: {
-      require:true,
-    }
-  }
-
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
-
-
-
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
